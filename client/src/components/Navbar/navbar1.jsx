69b09c37f096d6ad0c82f9b1f8bc8aeae2f235b6
vijay <vavijay333@gmail.com>
2022-09-17 18:01:58 +0530

    nav updated

--- client/src/components/Navbar/navbar1.jsx
+++ client/src/components/Navbar/navbar1.jsx
@@ -10,12 +10,15 @@ function Navbar()
       let flag1 = false;
       let flag2 = false;
       let flag3 = false;
-function solutions()
+function solutions(event)
 {
  
+  console.log("solutions1")
 
-  
+  console.log(event,"event")
    if(flag){
+
+    console.log("solutions",flag)
  document.querySelector(".active2").style.display="none";
  flag=false;
    }
@@ -26,7 +29,7 @@ else
              document.querySelector(".active4").style.display="none";
      document.querySelector(".active5").style.display="none";
      flag=true;
-      flag1 = false;
+    flag1 = false;
        flag2 = false;
       flag3 = false;
 }
@@ -82,6 +85,31 @@ else
 
 }
 
+
+window.onclick = function(event) {
+  // document.querySelector(".active2").style.display="none";
+  //         document.querySelector(".active3").style.display="none";
+  //            document.querySelector(".active4").style.display="none";
+  //    document.querySelector(".active5").style.display="none";
+   
+     console.log(event.target)
+     
+
+       let s1= document.querySelector(".active2").style;
+
+     
+
+       if(s1.display==="block" && event.target.name!=="solution"){
+             
+         console.log("solutions1")
+       console.log(s1.display,"s");
+           s1.display="none";
+           flag=false;
+
+       }
+
+   
+}
 function customer()
 {
  
@@ -119,28 +147,28 @@ else
 <div class="topnav1" id="myTopnav">
   <a href="#home" class="active"><img className="image" src="https://sarvamsafety.com/image/catalog/logo1.png" alt=""></img></a>
    <div class="dropdown">
-    <button onClick={solutions} class="dropbtn">SOLUTIONS<VscChevronDown style={{marginLeft:175 ,marginTop:-12}}/>
+    <button onClick={solutions} name="solution" class="dropbtn">SOLUTIONS<VscChevronDown id="icon" style={{marginLeft:175 ,marginTop:-12}}/>
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
     </div>
   </div> 
     <div class="dropdown">
-    <button onClick={industry} class="dropbtn">INDUSTRIES<VscChevronDown style={{marginLeft:180,marginTop:-12}}/>
+    <button onClick={industry} key="" class="dropbtn">INDUSTRIES<VscChevronDown id="icon" style={{marginLeft:180,marginTop:-12}}/>
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
     </div>
   </div> 
   <div class="dropdown">
-    <button onClick={about}  class="dropbtn">ABOUT <VscChevronDown style={{marginLeft:155,marginTop:-12}}/>
+    <button onClick={about}  class="dropbtn">ABOUT <VscChevronDown id="icon" style={{marginLeft:155,marginTop:-12}}/>
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
     </div>
   </div> 
   <div class="dropdown">
-    <button onClick={customer} class="dropbtn">CUSTOMER PROJECTS<VscChevronDown style={{marginLeft:225,marginTop:-15}}/>
+    <button onClick={customer} class="dropbtn">CUSTOMER PROJECTS<VscChevronDown id="icon" style={{marginLeft:225,marginTop:-12}}/>
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
